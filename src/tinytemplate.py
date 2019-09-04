7f35e4e0e433d84c489ab44607e63f39e552c959
Junkai Zhang <junkai.zhang@watchguard.com>
2019-09-04 18:39:28 +0800

    feat: parse template to code to render

--- src/tinytemplate.py
+++ src/tinytemplate.py
@@ -2,36 +2,124 @@
 """
 import re
 
-from compiler import CodeBuilder
-
 TOKENPATTERN = r"(?s)({{.*?}}|{%.*?%}|{#.*?#})"
 LOGIC = "{%"
 EXPRESSION = "{{"
 COMMENT = "{#"
-class TinyTemplate:
-    def __init__(self):
-        self.code = CodeBuilder()
 
-    def _tokenize_templ(self, templ_file):
+# TODO: this one should be configurable
+INDENT_STEP = 4
+
+class CodeBuilder:
+    """Generate Python code
+    """
+    def __init__(self, indent=0):
+        self.code = []
+        self._indent_level = indent
+
+    def indent(self):
+        """ Increase the indent level """
+        self._indent_level += INDENT_STEP
+
+    def dedent(self):
+        """ Decrease the indent level """
+        self._indent_level -= INDENT_STEP
+
+    def __str__(self):
+        return "".join(str(s) for s in self.code)
+
+    def add_line(self, line):
+        """ add one line of code with indentation"""
+        self.code.extend([" " * self._indent_level, line, "\n"])
+
+    def add_section(self):
+        section = CodeBuilder(self._indent_level)
+        self.code.append(section)
+        return section
+
+    def get_globals(self):
+        code_globals = dict()
+        exec(str(self), code_globals)
+        return code_globals
+
+def do_dot(var, prop):
+    try:
+        return var.get(prop)
+    except KeyError:
+        return var[prop]
+
+def dummy_function(*args):
+    return 0
+
+class TinyTemplate:
+    def __init__(self, tepmplate, *contexts):
+        
+        self.template = tepmplate
+        self.all_variables = set()
+        self.loop_vars = set()
+        self.render_code = dummy_function
+
+        self.context = dict()
+        for context in contexts:
+            self.context.update(context)
+
+    def _tokenize_templ(self):
         content = ''
-        with open(templ_file, 'r') as fp:
+        with open(self.template, 'r') as fp:
             content = fp.read()
         return re.split(TOKENPATTERN, content)
     
-    def render_expression(self, value):
-        self.code.add_line(value)
-        print(self.code.code)
-        return str(self.code)
+    def _expr_code(self, expr):
+        self._variables(expr)
+        code = f"c_{expr}"
+        return code
 
-    def render(self, templ_file, context):
-        templ_tokens = self._tokenize_templ(templ_file)
+    def _variables(self, var_name):
+        # TODO: if var_name is a valid variable name?
+        self.all_variables.add(var_name)
         
+
+    def compiler(self):
+        _code = CodeBuilder()
+
+        _code.add_line("def render_code(context):")
+        _code.indent()
+        vars_code = _code.add_section()
+        _code.add_line("result = list()")
+        _code.add_line("extend_result = result.extend")
+        _code.add_line("append_result = result.append")
+        _code.add_line("to_str = str")
+
         buffer = list()
+        def flush_output():
+            if len(buffer) == 1:
+                _code.add_line(f"append_result['{buffer[0]}']")
+            elif len(buffer) > 1:
+                _code.add_line(f'extend_result([{", ".join(buffer)}])')
+            del buffer[:]
+
+        templ_tokens = self._tokenize_templ()
         for token in templ_tokens:
             if token.startswith(COMMENT):
                 continue
             if token.startswith(EXPRESSION):
-                buffer.extend(self.render_expression(context))
+                expr = self._expr_code(token[2:-2].strip())
+                buffer.append(f"to_str({expr})")
                 continue
-            buffer.append(token)
-        return buffer
+            else:
+                if token:
+                    buffer.append(repr(token))
+        flush_output()
+
+        vars_code.add_line('print("Im ok")')
+
+        _code.add_line("result_str = ''.join(result)")
+        _code.add_line("return result_str")
+        
+        return _code.code
+
+    def render(self, context=None):
+        render_context = self.context
+        if context:
+            self.context.update(context)
+        return self.render_code(render_context)
--- tests/test_template_evaluate_var.py
+++ tests/test_template_evaluate_var.py
@@ -4,15 +4,16 @@ import tinytemplate
 def test_template_s_tokenize_templ():
     template_file = './templates/templ_var_eval.html'
     user_name = "Junkai Zhang"
-    templ = tinytemplate.TinyTemplate()
-    output = templ._tokenize_templ(template_file)
+    templ = tinytemplate.TinyTemplate(template_file)
+    output = templ._tokenize_templ()
     print(output)
     assert "{{ user_name }}" in output and isinstance(output, list)
 
 def test_template_variable_evaluation():
     template_file = './templates/templ_var_eval.html'
     user_name = "Junkai Zhang"
-    templ = tinytemplate.TinyTemplate()
-    output = templ.render(template_file, user_name)
-    print(output)
-    assert user_name in output
+    templ = tinytemplate.TinyTemplate(template_file)
+    output = templ.compiler()
+    print()
+    print(''.join(str(s) for s in output))
+    # assert user_name in output
