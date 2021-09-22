016f8ccf8b0533fd3d2ed8bafe444f8c1e710700
Junkai Zhang <junkai.zhang@watchguard.com>
2021-09-22 10:39:15 +0800

    style: Add type hints

--- src/tinytemplate.py
+++ src/tinytemplate.py
@@ -2,6 +2,8 @@
 """
 import re
 import pathlib
+from __future__ import annotations # pospone evaluation for type hints
+from typing import Any, Dict, List, Mapping, MutableSet, NoReturn, Optional, Sequence
 
 TOKENPATTERN = r"(?s)({{.*?}}|{%.*?%}|{#.*?#})"
 
@@ -11,49 +13,49 @@ INDENT_STEP = 4
 # templates/ folder should be same as this file.
 TEMPLATE_DIR = pathlib.Path(__file__).resolve().parent
 
-class CodeBuilder:
+class CodeBlock:
     """Generate Python code
     """
-    def __init__(self, indent=0):
-        self.code = []
+    def __init__(self, indent: int = 0) -> None:
+        self.code: List[str] = []
         self._indent_level = indent
 
-    def indent(self):
+    def indent(self) -> None:
         """ Increase the indent level """
         self._indent_level += INDENT_STEP
 
-    def dedent(self):
+    def dedent(self) -> None:
         """ Decrease the indent level """
         self._indent_level -= INDENT_STEP
 
     def __str__(self):
         return "".join(str(s) for s in self.code)
 
-    def add_line(self, line):
+    def add_line(self, line: str) -> None:
         """ add one line of code with indentation"""
         self.code.extend([" " * self._indent_level, line, "\n"])
 
-    def add_section(self):
-        section = CodeBuilder(self._indent_level)
-        self.code.append(section)
+    def add_section(self) -> CodeBlock:
+        section = CodeBlock(self._indent_level)
+        self.code.append(str(section))
         return section
 
-    def get_globals(self):
-        code_globals = dict()
+    def get_globals(self) -> Dict[str, str]:
+        code_globals: Dict[str, str] = dict()
         source_code = str(self)
         print(source_code)
         exec(source_code, code_globals)
         return code_globals
 
 
-def parse_template_file(template):
+def parse_template_file(template: str) -> List[str]:
     content = ''
     with open(template) as fp:
         content = fp.read()
     return re.split(TOKENPATTERN, content)
 
 
-def dot(var, *properties):
+def dot(var: Mapping[str, Any], *properties: str) -> Any:
     """Parse x.y, or x.y.z while x is a dict
 
     The x.y.z is representing the structure of following
@@ -76,7 +78,7 @@ def dot(var, *properties):
     
     :param var: the key of a context
     :type var: str
-    :return: the value propery of var
+    :return: the value property of var
     :rtype: Any
     """
     for prop in properties:
@@ -88,21 +90,21 @@ def dot(var, *properties):
 
 
 class TinyTemplate:
-    def __init__(self, template, *contexts):
+    def __init__(self, template: str, *contexts):
         self._template = f'{str(TEMPLATE_DIR)}/templates/{template}'
         if pathlib.PurePath(template).is_absolute():
             self._template = template
 
-        self._buffer = list()
-        self._ops_stack = list()
+        self._buffer: List[Any] = list()
+        self._ops_stack: List[Any] = list()
 
-        self._context = dict()
-        self._all_variables = set()
-        self._loop_variables = set()
-        self._code = CodeBuilder()
+        self._context: Dict[str, Any] = dict()
+        self._all_variables: MutableSet[str] = set()
+        self._loop_variables: MutableSet[str] = set()
+        self._code = CodeBlock()
         self._templ_tokens = parse_template_file(self._template)
 
-    def render(self, context=None):
+    def render(self, context: Optional[Dict[str, Any]] = None):
         render_context = self._context
         if context:
             render_context.update(context)
@@ -110,12 +112,12 @@ class TinyTemplate:
         source_code = ''.join(str(s) for s in codeobj.code)
         print(source_code)
         # Use for storing globals in source_code_globals
-        source_code_globals = dict()
+        source_code_globals: Dict[str, Any] = dict()
         exec(source_code, source_code_globals)
         self.code_runner = source_code_globals['render_code']
         return self.code_runner(render_context, dot)
 
-    def _compile_to_pycode(self):
+    def _compile_to_pycode(self) -> CodeBlock:
         self._code.add_line("def render_code(context, dot):")
         self._code.indent()
         self._vars_code = self._code.add_section()
@@ -130,18 +132,18 @@ class TinyTemplate:
 
         return self._code
 
-    def _handling_global_variables(self):
+    def _handling_global_variables(self) -> None:
         for var_name in self._all_variables - self._loop_variables:
             self._vars_code.add_line(f'c_{var_name} = context["{var_name}"]')
 
-    def flush_buffer(self):
+    def flush_buffer(self) -> None:
         if len(self._buffer) == 1:
             self._code.add_line(f"append_result({self._buffer[0]})")
         elif len(self._buffer) > 1:
             self._code.add_line(f'extend_result([{", ".join(self._buffer)}])')
         del self._buffer[:]
 
-    def _template_token_analyze(self):
+    def _template_token_analyze(self) -> None:
         # FIXME: handle the damn multiple CRs, current output sucks.
         for token in self._templ_tokens:
             # Ignore comment lines
@@ -156,11 +158,11 @@ class TinyTemplate:
                     self._buffer.append(repr(token))
         self.flush_buffer()
 
-    def _handling_variable_evaluation(self, token):
+    def _handling_variable_evaluation(self, token: str) -> None:
         stmt_code = self._generate_statement_code(token[2:-2].strip())
         self._buffer.append(f"to_str({stmt_code})")
 
-    def _handling_logic_control_statements(self, token):
+    def _handling_logic_control_statements(self, token: str) -> None:
         self.flush_buffer()
         # FIXME: we only support one condition evaluation
         # Extract statement first, [2:-2] to strip {%...%} marker
@@ -173,7 +175,7 @@ class TinyTemplate:
         elif op.startswith('end'):
             self._end_statement()
 
-    def _if_statement(self, expressions):
+    def _if_statement(self, expressions: List[str]) -> None:
         if len(expressions) == 0:
             raise SyntaxError(f"Unknown syntax: if expression: `if {' '.join(expressions)}`")
         expr = self._generate_statement_code(' '.join(expressions))
@@ -181,7 +183,7 @@ class TinyTemplate:
         self._code.add_line(f"if {expr}:")
         self._code.indent()
 
-    def _for_statement(self, expressions):
+    def _for_statement(self, expressions: List[str]) -> None:
         # for loop should be `for x in y`
         if len(expressions) < 3 or expressions[1] != 'in':
             raise SyntaxError(f"Unknown syntax: for expression: `if {' '.join(expressions)}`")
@@ -193,11 +195,11 @@ class TinyTemplate:
         self._code.add_line(f"for c_{loop_var} in {self._generate_statement_code(iter_var)}:")
         self._code.indent()
 
-    def _end_statement(self):
+    def _end_statement(self) -> None:
         # TODO: end_op = op[3:] need error handling
         self._code.dedent()
 
-    def _generate_dot_code(self, expr):
+    def _generate_dot_code(self, expr: str) -> str:
         """Convert "var.property" to "dot(var, "property")"
 
         Multiple dots supported, too.
@@ -218,7 +220,7 @@ class TinyTemplate:
         args = ", ".join(repr(d) for d in property_)
         return f"dot(c_{var}, {args})"
 
-    def _generate_pipe_code(self, expr):
+    def _generate_pipe_code(self, expr: str) -> str:
         """Convert "var|func1|func2" to "c_func2(c_func1(c_var))"
 
         :param expr: pipe variables
@@ -233,7 +235,7 @@ class TinyTemplate:
             code = f"c_{_func}({code})"
         return code
 
-    def _generate_var_code(self, expr):
+    def _generate_var_code(self, expr: str) -> str:
         """Convert "var" to "c_var"
 
         :param expr: variable name
@@ -244,7 +246,7 @@ class TinyTemplate:
         self._add_all_variables(expr)
         return f"c_{expr}"
 
-    def _generate_statement_code(self, expr):
+    def _generate_statement_code(self, expr: str) -> str:
         code = ''
         # `|`` pipes must be parsed before `.` dots parsed due to evaluation order
         if '|' in expr:
@@ -255,10 +257,10 @@ class TinyTemplate:
             code = self._generate_var_code(expr)
         return code
 
-    def _add_all_variables(self, var_name):
+    def _add_all_variables(self, var_name: str) -> None:
         # TODO: if var_name is a valid variable name?
         self._all_variables.add(var_name)
 
-    def _add_loop_variables(self, var_name):
+    def _add_loop_variables(self, var_name: str) -> None:
         self._loop_variables.add(var_name)
 
